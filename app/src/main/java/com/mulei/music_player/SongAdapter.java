156ee23a848e97b0e360a8d45949d40c8b9bcdc1
Mukshi <brianmulei0@gmail.com>
2020-02-20 18:20:16 +0300

    adapters and listeners

--- app/src/main/java/com/mulei/music_player/SongAdapter.java
+++ app/src/main/java/com/mulei/music_player/SongAdapter.java
@@ -1,4 +1,31 @@
 package com.mulei.music_player;
 
-public class SongAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongHolder> {
+    @NonNull
+    @Override
+    public SongHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SongHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class SongHolder extends RecyclerView.ViewHolder {
+        public SongHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
--- app/src/main/java/com/mulei/music_player/SongInfo.java
+++ app/src/main/java/com/mulei/music_player/SongInfo.java
@@ -1,4 +1,14 @@
 package com.mulei.music_player;
 
 public class SongInfo {
+    public String songname,artistName,songUrl;
+
+    public SongInfo() {
+    }
+
+    public SongInfo(String songname, String artistName, String songUrl) {
+        this.songname = songname;
+        this.artistName = artistName;
+        this.songUrl = songUrl;
+    }
 }
